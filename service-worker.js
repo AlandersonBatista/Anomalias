45a051de4f71f58a28f925858a2809559329b8cb
Alanderson Batista <alanderson.c@gmail.com>
2025-11-25 17:46:18 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'sigma-anomalias-calc-style-v1';
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './css/styles.css',
+  './js/app.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
